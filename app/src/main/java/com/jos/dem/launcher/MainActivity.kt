4ad827028a173d23fa27dba3ba911c3db669e64e
josdem <joseluis.delacruz@gmail.com>
2021-08-26 11:41:43 -0400

    Adding button clicked action with a toast

--- app/src/main/java/com/jos/dem/launcher/MainActivity.kt
+++ app/src/main/java/com/jos/dem/launcher/MainActivity.kt
@@ -2,10 +2,18 @@ package com.jos.dem.launcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Clicked Go!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
