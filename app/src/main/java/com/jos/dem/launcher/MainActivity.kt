845abaa416991198c2f926f9be4c6b8fc548cf75
martin-applitools <martin.kowalewski@applitools.com>
2023-01-27 16:40:35 -0600

    Appium Helper Library and Update NMG Library

--- app/src/main/java/com/jos/dem/launcher/MainActivity.kt
+++ app/src/main/java/com/jos/dem/launcher/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.applitools.appium.helper.androidx.EyesAppiumHelper
 import com.applitools.vhs.EyesUfg
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        EyesAppiumHelper.initialize(this.getApplication());
         EyesUfg.setupAppium(this.getApplication())
         val button = findViewById<Button>(R.id.button)
         val message = findViewById<TextView>(R.id.message)
